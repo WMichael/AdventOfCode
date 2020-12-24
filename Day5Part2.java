df268849b6cf10d8b9b3575bf82739106babf6b0
Michael Weller <vve11ermichae1@gmail.com>
2020-12-24 17:52:59 +0000

    Completed Day 5 Part 2

--- Day5Part2.java
+++ Day5Part2.java
@@ -1,12 +1,15 @@
 import java.io.BufferedReader;
 import java.io.FileReader;
 import java.io.IOException;
+import java.util.Arrays;
 
-public class Day5Part1 {
+public class Day5Part2 {
 
     public static void main(String[] args) throws IOException {
         var file = "Resources/day5actual.txt";
         int highestSeatID = 0;
+        Boolean[][] seats = new Boolean[128][8];
+        Arrays.stream(seats).forEach(a -> Arrays.fill(a, Boolean.FALSE));
 
         try (BufferedReader br = new BufferedReader(new FileReader(file))) {
             
@@ -45,12 +48,29 @@ public class Day5Part1 {
 
                 // Calculate seatID
                 seatID = (row * 8) + column;
-    
-            // System.out.printf("Row: %d, Column: %d, Seat ID: %d \n", row, column, seatID );
+                
+                // Set the highestSeatID
                if(seatID > highestSeatID) { highestSeatID = seatID;}
+
+               // Set seat in seats array to true for row & column
+               seats[row][column] = true;
+
+               
             }
 
-            System.out.printf("Highest SeatID is %d \n", highestSeatID);
+            // Print the Row, Column and Seat ID of all empty seats
+            System.out.println("Empty seats: ");
+            for (int i = 0; i < seats.length; i++) {
+                for (int j = 0; j < seats[i].length; j++) {
+                    if(!seats[i][j] && seats[i][j-1] && seats[i][j+1]) {
+                        System.out.printf("Row: %d, Column: %d, Seat ID: %d \n", i, j, (i * 8) + j);
+
+                        // Shows all empty seats, but the correct seat will be the seat with a seat filled before and after
+                        // the correct one. 
+                    }
+                }
+            }
         }
+
     }
 }
